7bf2dcfa1ed783574edeba6cc8ca766dda4e610c
Rigved Epur <rigved.epur@gmail.com>
2024-02-17 23:47:04 -0800

    working backyard flyer

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -23,7 +23,8 @@ class BackyardFlyer(Drone):
     def __init__(self, connection):
         super().__init__(connection)
         self.target_position = np.array([0.0, 0.0, 0.0])
-        self.all_waypoints = []
+        self.altitude = 3.0 # Take off altitude
+        self.all_waypoints = self.calculate_box(box_length=10)
         self.in_mission = True
         self.check_state = {}
 
@@ -39,9 +40,36 @@ class BackyardFlyer(Drone):
         """
         TODO: Implement this method
 
+
         This triggers when `MsgID.LOCAL_POSITION` is received and self.local_position contains new data
         """
-        pass
+        if self.flight_state == States.TAKEOFF:
+
+            # coordinate conversion
+            altitude = -1.0 * self.local_position[2]
+
+            # check if altitude is within 95% of target
+            if altitude > 0.95 * self.target_position[2]:
+                self.waypoint_transition()
+
+        elif self.flight_state == States.WAYPOINT:
+
+            # Calculate euclidean distance between current position and target position
+            distance = np.linalg.norm(self.target_position[0:2] - self.local_position[0:2])
+
+            # print(self.local_position, self.target_position, distance, len(self.all_waypoints))
+
+            # checking to see if drone's position is within 1m of target location
+            if distance < 1:
+
+                # Check if all way points are covered
+                if len(self.all_waypoints) > 0:
+                    self.waypoint_transition()
+                else:
+                    self.landing_transition()  # Land if all waypoints are covered
+
+
+
 
     def velocity_callback(self):
         """
@@ -49,7 +77,10 @@ class BackyardFlyer(Drone):
 
         This triggers when `MsgID.LOCAL_VELOCITY` is received and self.local_velocity contains new data
         """
-        pass
+        if self.flight_state == States.LANDING:
+            if ((self.global_position[2] - self.global_home[2] < 0.1) and
+                    abs(self.local_position[2]) < 0.01):
+                self.disarming_transition()
 
     def state_callback(self):
         """
@@ -57,14 +88,33 @@ class BackyardFlyer(Drone):
 
         This triggers when `MsgID.STATE` is received and self.armed and self.guided contain new data
         """
-        pass
+        if not self.in_mission:
+            return
+        if self.flight_state == States.MANUAL:
+            self.arming_transition()
+        elif self.flight_state == States.ARMING:
+            self.takeoff_transition()
+        elif self.flight_state == States.DISARMING:
+            self.manual_transition()
+        elif self.flight_state == States.LANDING:
+            self.disarming_transition()
 
-    def calculate_box(self):
+
+    def calculate_box(self, box_length):
         """TODO: Fill out this method
         
         1. Return waypoints to fly a box
         """
-        pass
+
+
+
+        return [
+            [box_length, 0, self.altitude],
+            [box_length, box_length, self.altitude],
+            [0, box_length, self.altitude],
+            [0, 0, self.altitude]
+        ]
+
 
     def arming_transition(self):
         """TODO: Fill out this method
@@ -75,6 +125,17 @@ class BackyardFlyer(Drone):
         4. Transition to the ARMING state
         """
         print("arming transition")
+        self.take_control()
+        self.arm()
+
+        self.starting_position = self.global_position
+
+        self.set_home_position(self.global_position[0],
+                               self.global_position[1],
+                               self.global_position[2])
+
+        self.flight_state = States.ARMING
+
 
     def takeoff_transition(self):
         """TODO: Fill out this method
@@ -84,6 +145,10 @@ class BackyardFlyer(Drone):
         3. Transition to the TAKEOFF state
         """
         print("takeoff transition")
+        self.target_position[2] = self.altitude
+        self.takeoff(self.altitude)
+
+        self.flight_state = States.TAKEOFF
 
     def waypoint_transition(self):
         """TODO: Fill out this method
@@ -93,6 +158,19 @@ class BackyardFlyer(Drone):
         """
         print("waypoint transition")
 
+        # Get the first waypoint position from the list
+        self.target_position = self.all_waypoints[0]  #
+        del self.all_waypoints[0] # Delete this waypoint
+
+        print('target position', self.target_position)
+
+        # Command the drone to go that waypoint
+        self.cmd_position(self.target_position[0], self.target_position[1], self.target_position[2], 0.0)
+        self.flight_state = States.WAYPOINT
+
+
+
+
     def landing_transition(self):
         """TODO: Fill out this method
         
@@ -100,6 +178,8 @@ class BackyardFlyer(Drone):
         2. Transition to the LANDING state
         """
         print("landing transition")
+        self.land()
+        self.flight_state = States.LANDING
 
     def disarming_transition(self):
         """TODO: Fill out this method
@@ -108,6 +188,8 @@ class BackyardFlyer(Drone):
         2. Transition to the DISARMING state
         """
         print("disarm transition")
+        self.disarm()
+        self.flight_state = States.DISARMING
 
     def manual_transition(self):
         """This method is provided
@@ -118,6 +200,16 @@ class BackyardFlyer(Drone):
         4. Transition to the MANUAL state
         """
         print("manual transition")
+
+        self.end_position = self.global_position
+
+        print(f'Starting Position - {self.starting_position}')
+        print(f'Ending Position - {self.end_position}')
+        distance = np.linalg.norm(self.starting_position-self.end_position)
+        if distance < 1.0:
+            print(f'Mission Passed - Distance between start and final position = {distance:.3} m')
+        else:
+            print(f'Mission Failed - Distance between start and final position = {distance:.3} m')
 
         self.release_control()
         self.stop()
